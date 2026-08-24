12a800cae1d0d66c6125316f75af1cc9889be802
magagne <MatthieuGagne@gmail.com>
2026-08-23 22:01:19 -0400

    From https://github.com/magagne/Ploopy-Nano-2-VIAL-ScrollLock  - [deleted]               (none)     -> origin/HEAD 2 fatal: bad object refs/remotes/origin/HEAD 2 error: https://github.com/magagne/Ploopy-Nano-2-VIAL-ScrollLock.git did not send all necessary objects
    Add Raw HID receiver for drag scroll

--- keyboards/ploopyco/nano_2/rev2_003/keymaps/via_scrolllock/keymap.c
+++ keyboards/ploopyco/nano_2/rev2_003/keymaps/via_scrolllock/keymap.c
@@ -1,8 +1,32 @@
-
 /* MATTHIEU */
 
 #include QMK_KEYBOARD_H
+#include <raw_hid.h>
+
+extern bool is_drag_scroll;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(DPI_CONFIG)
 };
+
+void raw_hid_receive(uint8_t *data, uint8_t length) {
+    if (length < 1) {
+        return;
+    }
+
+    switch (data[0]) {
+        case 'S':
+            is_drag_scroll = true;
+            dprintf("RAW HID: DRAG_SCROLL ON\n");
+            break;
+
+        case 's':
+            is_drag_scroll = false;
+            dprintf("RAW HID: DRAG_SCROLL OFF\n");
+            break;
+
+        default:
+            dprintf("RAW HID: unknown command 0x%02X\n", data[0]);
+            break;
+    }
+}
