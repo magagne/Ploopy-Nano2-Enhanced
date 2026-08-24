c0b82bc09ba79a1e636fe6cdbeaac1870583ed64
magagne <MatthieuGagne@gmail.com>
2026-08-23 22:31:57 -0400

    Fix Raw HID receiver to use VIA command hook

--- keyboards/ploopyco/nano_2/rev2_003/keymaps/via_scrolllock/keymap.c
+++ keyboards/ploopyco/nano_2/rev2_003/keymaps/via_scrolllock/keymap.c
@@ -1,6 +1,7 @@
 /* MATTHIEU */
 
 #include QMK_KEYBOARD_H
+
 #include <raw_hid.h>
 
 extern bool is_drag_scroll;
@@ -9,24 +10,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(DPI_CONFIG)
 };
 
-void raw_hid_receive(uint8_t *data, uint8_t length) {
-    if (length < 1) {
-        return;
-    }
-
+bool via_command_kb(uint8_t *data, uint8_t length) {
     switch (data[0]) {
         case 'S':
             is_drag_scroll = true;
             dprintf("RAW HID: DRAG_SCROLL ON\n");
-            break;
+            return true;
 
         case 's':
             is_drag_scroll = false;
             dprintf("RAW HID: DRAG_SCROLL OFF\n");
-            break;
+            return true;
 
         default:
-            dprintf("RAW HID: unknown command 0x%02X\n", data[0]);
-            break;
+            return false;
     }
 }
